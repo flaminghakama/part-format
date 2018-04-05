34e5b04d90d2b581c6210040b4ef0d77bbad805f
David Elaine Alt <elaine@flaminghakama.com>
2018-04-05 12:08:30 -0700

    added 7page script and 5-3 combine

---
+++ bookify-7page.py
@@ -0,0 +1,25 @@
+# bookify-7page.py
+
+# bookify-7page.py input1.pdf output1.pdf
+
+import sys
+from PyPDF2 import PdfFileWriter, PdfFileReader, PdfFileMerger
+
+inputOneFileName = sys.argv[1] 
+outputOneFileName = sys.argv[2] 
+print "input 1: " + inputOneFileName
+print "output 1: " + outputOneFileName 
+
+inputReaderOne = PdfFileReader(open(inputOneFileName, "rb"))
+outputWriterOne = PdfFileWriter()
+
+outputWriterOne.addPage(inputReaderOne.getPage(0))
+outputWriterOne.addPage(inputReaderOne.getPage(5))
+outputWriterOne.addPage(inputReaderOne.getPage(2))
+outputWriterOne.addPage(inputReaderOne.getPage(3))
+outputWriterOne.addPage(inputReaderOne.getPage(4))
+outputWriterOne.addPage(inputReaderOne.getPage(1))
+outputWriterOne.addPage(inputReaderOne.getPage(6))
+outputWriterOne.insertBlankPage()
+outputStreamOne = file(outputOneFileName, "wb")
+outputWriterOne.write(outputStreamOne)
---
+++ combine-5page-3page.py
@@ -0,0 +1,29 @@
+# halfpagify-5page-3page.py
+
+# halfpagify-5page-3page.py input1.pdf input2.pdf output.pdf
+# combine two files, so that their last pages split a tabloid page
+
+import sys
+from PyPDF2 import PdfFileWriter, PdfFileReader, PdfFileMerger
+
+inputOneFileName = sys.argv[1] 
+inputTwoFileName = sys.argv[2] 
+outputOneFileName = sys.argv[3] 
+print "input 1: " + inputOneFileName
+print "input 2: " + inputTwoFileName 
+print "output 1: " + outputOneFileName 
+
+inputReaderOne = PdfFileReader(open(inputOneFileName, "rb"))
+inputReaderTwo = PdfFileReader(open(inputTwoFileName, "rb"))
+outputWriterOne = PdfFileWriter()
+
+outputWriterOne.addPage(inputReaderOne.getPage(0))
+outputWriterOne.addPage(inputReaderOne.getPage(1))
+outputWriterOne.addPage(inputReaderOne.getPage(2))
+outputWriterOne.addPage(inputReaderOne.getPage(3))
+outputWriterOne.addPage(inputReaderOne.getPage(4))
+outputWriterOne.addPage(inputReaderTwo.getPage(2))
+outputWriterOne.addPage(inputReaderTwo.getPage(0))
+outputWriterOne.addPage(inputReaderTwo.getPage(1))
+outputStreamOne = file(outputOneFileName, "wb")
+outputWriterOne.write(outputStreamOne)
