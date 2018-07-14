1741fa72be3f12d7fe757d6ee4d4b89f91115421
David Elaine Alt <elaine@flaminghakama.com>
2018-07-13 19:58:10 -0700

    adding command line option for page order

--- bookify-7page.py
+++ bookify-7page.py
@@ -1,23 +1,32 @@
 # bookify-7page.py
 
-# bookify-7page.py input1.pdf output1.pdf
+# bookify-7page.py input1.pdf output1.pdf [automatic|manual]
 
 #
-#  _____1__________________ -_____  
-#        2       /\       7
-#               /  \
-#            3 /    \ 6
-#             / 4  5 \
-#            /        \
-#           /          \
+#  ____1_________________ -_____  
+#       2       /\       7
+#              /  \
+#           3 /    \ 6
+#            / 4  5 \
+#           /        \
+#          /          \
 #
 
-#  Printed in order:  
+#  The order of pages is based on the 3rd argument.
+
+#  If the 3rd argument is specified as "manual", 
+#  the printing order is for manual double-sided printing, 
+#  with both outward sides printed first, then the inward sides"
 #    Outward outside
 #    Outward inside
 #    Inward outside
 #    Inward inside
-
+ 
+#  Otherwise, the printing order is intended for automatic double-sided printing:
+#    Outward outside
+#    Inward outside
+#    Outward inside
+#    Inward inside
 
 import sys
 from PyPDF2 import PdfFileWriter, PdfFileReader, PdfFileMerger
@@ -30,13 +39,31 @@ print "output 1: " + outputOneFileName
 inputReaderOne = PdfFileReader(open(inputOneFileName, "rb"))
 outputWriterOne = PdfFileWriter()
 
-outputWriterOne.addPage(inputReaderOne.getPage(0))
-outputWriterOne.addPage(inputReaderOne.getPage(5))
-outputWriterOne.addPage(inputReaderOne.getPage(2))
-outputWriterOne.addPage(inputReaderOne.getPage(1))
-outputWriterOne.addPage(inputReaderOne.getPage(6))
-outputWriterOne.addPage(inputReaderOne.getPage(3))
-outputWriterOne.addPage(inputReaderOne.getPage(4))
+pageIndex = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+pageIndex[2] = 0
+
+if sys.argv[3] == "manual":
+	pageIndex[3] = 5
+	pageIndex[4] = 2
+	pageIndex[5] = 1
+	pageIndex[6] = 6
+
+else:
+	pageIndex[3] = 1
+	pageIndex[4] = 6
+	pageIndex[5] = 5
+	pageIndex[6] = 2
+
+pageIndex[7] = 3 
+pageIndex[8] = 4
+
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[2]))
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[3]))
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[4]))
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[5]))
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[6]))
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[7]))
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[8]))
 outputWriterOne.insertBlankPage()
 outputStreamOne = file(outputOneFileName, "wb")
 outputWriterOne.write(outputStreamOne)
--- bookify-8page.py
+++ bookify-8page.py
@@ -1,21 +1,31 @@
 # bookify-8page.py
 
-# bookify-8page.py input1.pdf output1.pdf
+# bookify-8page.py input1.pdf output1.pdf [automatic|manual]
 
 #
-#  _____1___________________8____
-#        2       /\       7
-#               /  \
-#            3 /    \ 6
-#             / 4  5 \
-#            /        \
-#           /          \
+#  ____1__________________8____
+#       2       /\       7
+#              /  \
+#           3 /    \ 6
+#            / 4  5 \
+#           /        \
+#          /          \
 #
 
-#  Printed in the order:  
+#  The order of pages is based on the 3rd argument.
+
+#  If the 3rd argument is specified as "manual", 
+#  the printing order is for manual double-sided printing, 
+#  with both outward sides printed first, then the inward sides
 #    Outward outside
 #    Outward inside
 #    Inward outside
+#    Inward inside
+ 
+#  Otherwise, the printing order is intended for automatic double-sided printing:
+#    Outward outside
+#    Inward outside
+#    Outward inside
 #    Inward inside
 
 import sys
@@ -29,13 +39,32 @@ print "output 1: " + outputOneFileName
 inputReaderOne = PdfFileReader(open(inputOneFileName, "rb"))
 outputWriterOne = PdfFileWriter()
 
-outputWriterOne.addPage(inputReaderOne.getPage(7))
-outputWriterOne.addPage(inputReaderOne.getPage(0))
-outputWriterOne.addPage(inputReaderOne.getPage(5))
-outputWriterOne.addPage(inputReaderOne.getPage(2))
-outputWriterOne.addPage(inputReaderOne.getPage(1))
-outputWriterOne.addPage(inputReaderOne.getPage(6))
-outputWriterOne.addPage(inputReaderOne.getPage(3))
-outputWriterOne.addPage(inputReaderOne.getPage(4))
+pageIndex = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+pageIndex[1] = 7
+pageIndex[2] = 0
+
+if sys.argv[3] == "manual":
+	pageIndex[3] = 5
+	pageIndex[4] = 2
+	pageIndex[5] = 1
+	pageIndex[6] = 6
+
+else:
+	pageIndex[3] = 1
+	pageIndex[4] = 6
+	pageIndex[5] = 5
+	pageIndex[6] = 2
+
+pageIndex[7] = 3 
+pageIndex[8] = 4
+
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[1]))
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[2]))
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[3]))
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[4]))
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[5]))
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[6]))
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[7]))
+outputWriterOne.addPage(inputReaderOne.getPage(pageIndex[8]))
 outputStreamOne = file(outputOneFileName, "wb")
 outputWriterOne.write(outputStreamOne)
